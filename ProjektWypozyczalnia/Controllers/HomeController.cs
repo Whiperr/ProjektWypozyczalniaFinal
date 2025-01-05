ab1b8738727fe22f06851c3a30bd02606e241e7d
Marcin <marcin.karas2@wp.pl>
2025-01-05 15:32:25 +0100

    poprawki

--- ProjektWypozyczalnia/Controllers/HomeController.cs
+++ ProjektWypozyczalnia/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using ProjektWypozyczalnia.Models;
 using System.Diagnostics;
 
 
+
 namespace ProjektWypozyczalnia.Controllers
 {
     public class HomeController : Controller
@@ -22,10 +23,11 @@ namespace ProjektWypozyczalnia.Controllers
         {
             // Pobierz 5 samochodów z bazy danych
             var cars = await _context.Car
-                .Include(c => c.Model) // Jeœli chcesz za³¹czyæ dane modelu
+                .Include(c => c.Model)// Jeœli chcesz za³¹czyæ dane modelu
+                .ThenInclude(m => m.Manufacturer) 
                 .Include(c => c.Type)// Jeœli chcesz za³¹czyæ typ samochodu
                 .Include(c => c.Reservations)       // Za³aduj rezerwacje
-                .ThenInclude(r => r.Status) 
+                .ThenInclude(r => r.Status)
                 .Take(5)
                 .ToListAsync();
 
